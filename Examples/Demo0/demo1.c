4b62070237d6cba78848505e2b755944b636674e
Antonio Capece <s282685@studenti.polito.it>
2023-12-10 12:26:06 +0100

    Solved demo1 problems

--- Examples/Demo0/demo1.c
+++ Examples/Demo0/demo1.c
@@ -8,6 +8,7 @@
 #include "semphr.h"
 
 #define MAX_TASKS 3
+#define MAX_COUNT 1
 
 SemaphoreHandle_t xSemaphores[MAX_TASKS];
 
@@ -19,12 +20,11 @@ void vTask3() {
     while(1) {
         if (xSemaphoreTake(xSemaphores[2], portMAX_DELAY) == pdTRUE) {
             printf("After 3 seconds...\n");
+            vTaskDelay(pdMS_TO_TICKS(3000));
             printf("Wake up: semaphore 3\n");
             xSemaphoreGive(xSemaphores[0]);
         }
 
-        vTaskDelay(pdMS_TO_TICKS(1000));
-        vTask();
     }
 }
 
@@ -32,12 +32,11 @@ void vTask2() {
     while(1) {
         if (xSemaphoreTake(xSemaphores[1], portMAX_DELAY) == pdTRUE) {
             printf("After 2 seconds...\n");
+            vTaskDelay(pdMS_TO_TICKS(2000));
             printf("Wake up: semaphore 2\n");
             xSemaphoreGive(xSemaphores[2]);
         }
 
-        vTaskDelay(pdMS_TO_TICKS(3000));
-        vTask3();
     }
 }
 
@@ -45,24 +44,26 @@ void vTask() {
     while(1) {
         if (xSemaphoreTake(xSemaphores[0], portMAX_DELAY) == pdTRUE) {
             printf("After 1 second...\n");
+            vTaskDelay(pdMS_TO_TICKS(1000));
             printf("Wake up: semaphore 1\n");
             xSemaphoreGive(xSemaphores[1]);
         }
 
-        vTaskDelay(pdMS_TO_TICKS(2000));
-        vTask2();
     }
 }
 
 void demo1() {
     for (int i = 0; i < MAX_TASKS; i++) {
-        xSemaphores[i] = xSemaphoreCreateMutex();
+        xSemaphores[i] = xSemaphoreCreateCounting(MAX_COUNT, 0);
         if (xSemaphores[i] == NULL) {
             printf("ERROR: bad semaphore creation.\n");
         }
     }
+    xSemaphoreGive(xSemaphores[0]);
     
     xTaskCreate(vTask, "Semaphore", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(vTask2, "Semaphore", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
+    xTaskCreate(vTask3, "Semaphore", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
 
     /* Starting FreeRTOS scheduler */
     vTaskStartScheduler();
