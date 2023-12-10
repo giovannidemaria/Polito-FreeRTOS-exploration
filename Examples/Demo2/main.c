43e1ec4e691f7902723b64f5faf4e9d135a6ba67
giovannidemaria <giovidemaria@gmail.com>
2023-12-10 19:14:23 +0100

    funzioni definite come macro (consiglio anto)

--- Examples/Demo2/main.c
+++ Examples/Demo2/main.c
@@ -312,13 +312,3 @@ void *malloc( size_t size )
 
 }
 
-void portCONFIGURE_TIMER_FOR_RUN_TIME_STATS(void){
-    /* Example using SysTick as the timer */
-    /* Configure SysTick to interrupt at a specific frequency */
-    SysTick_Config(SystemCoreClock / 1000); // Example: SysTick at 1 ms
-}
-
-unsigned long portGET_RUN_TIME_COUNTER_VALUE(void){
-    /* Return the SysTick current value. */
-    return SysTick->VAL;
-}
