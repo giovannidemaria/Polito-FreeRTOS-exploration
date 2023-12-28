b44a63afc3c3f913743f12c25ba20475b00e91fd
n4rciso <narcisocyber@gmail.com>
2023-12-28 12:52:46 +0100

    Descrizione semplice Demo3

--- Examples/Demo3/demo3.c
+++ Examples/Demo3/demo3.c
@@ -1,75 +1,29 @@
-/* Standard includes. */
-#include <stdio.h>
-
-/* Kernel includes. */
-#include "FreeRTOS.h"
-#include "task.h"
-#include "timers.h"
-#include "semphr.h"
-
-#define MAX_TASKS 3
-#define MAX_COUNT 1
-
-SemaphoreHandle_t xSemaphores[MAX_TASKS];
-
-void vTask();
-void vTask2();
-void vTask3();
-
-void vTask3() {
-    while(1) {
-        if (xSemaphoreTake(xSemaphores[2], portMAX_DELAY) == pdTRUE) {
-            printf("After 3 seconds...\n");
-            vTaskDelay(pdMS_TO_TICKS(3000));
-            printf("Wake up: semaphore 3\n");
-            xSemaphoreGive(xSemaphores[0]);
-        }
-
-    }
-}
-
-void vTask2() {
-    while(1) {
-        if (xSemaphoreTake(xSemaphores[1], portMAX_DELAY) == pdTRUE) {
-            printf("After 2 seconds...\n");
-            vTaskDelay(pdMS_TO_TICKS(2000));
-            printf("Wake up: semaphore 2\n");
-            xSemaphoreGive(xSemaphores[2]);
-        }
-
-    }
-}
-
-void vTask() {
-    while(1) {
-        if (xSemaphoreTake(xSemaphores[0], portMAX_DELAY) == pdTRUE) {
-            printf("After 1 second...\n");
-            vTaskDelay(pdMS_TO_TICKS(1000));
-            printf("Wake up: semaphore 1\n");
-            xSemaphoreGive(xSemaphores[1]);
-        }
-
-    }
-}
-
-void demo3() {
-    for (int i = 0; i < MAX_TASKS; i++) {
-        xSemaphores[i] = xSemaphoreCreateCounting(MAX_COUNT, 0);
-        if (xSemaphores[i] == NULL) {
-            printf("ERROR: bad semaphore creation.\n");
-        }
-    }
-    xSemaphoreGive(xSemaphores[0]);
+/*  Demo 3
+    Gestione pronto soccorso
     
-    xTaskCreate(vTask, "Semaphore", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-    xTaskCreate(vTask2, "Semaphore", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-    xTaskCreate(vTask3, "Semaphore", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 1, NULL);
-
-    /* Starting FreeRTOS scheduler */
-    vTaskStartScheduler();
+    L'ospedale ha "MAX_SALE" disponibili per operare i pazienti
+    ogni sala opera in parallelo rispetto alle altre.
     
-    for(;;);
-}
+    Ai pazienti viene assegnato un colore che identifica la gravità e un tempo entro il quale
+    se l'operazione non viene conclusa la sitauzione del paziente si aggrava:
+    
+            codice verde --> codice arancione (10 secondi)
+            codice arancione --> codice rosso (5 secondi)
+            codice rosso --> paziente morto (2.5 secondi)
+
+    
+    Ipotesi: 
+        - quando un paziente entra in una delle sale operatorie, non può uscire prima che 
+    l'operazione sia terminata
+        - per semplicità ad ogni codice è assegnato un tempo di durata dell'operazione 
+        - i pazienti vengono operati per prorità di colore e differenza tra tempo necessario all'operazione
+        di operazione e tempo di arrivo 
 
 
-// modifica per vedere se riesco a creare il mio branch
+
+    Le sale operatorie sono identificate come una coda di MAX_SALE elementi
+
+    I pazienti sono rappresentati con tre code (in base al colore associato al paziente)
+
+    Attraverso i semafori si gestisce quale delle code dei pazienti entra in sala
+*/
