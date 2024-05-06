5533a8b3d11d93e93143d9a5adbaf30ed16ab46f
efsi28 <federicocagnazzo01@gmail.com>
2024-05-06 23:42:43 +0200

    demo03.c print all zeros

--- Examples/Demo3/demo3.c
+++ Examples/Demo3/demo3.c
@@ -16,81 +16,92 @@ but the same code, could be written for an undefined number of repetition of pri
 #include "task.h"
 
 #define DIM 4
+#define ROW 4
+#define COL 4
 
 typedef struct s_mat{
-    int vA[DIM];            //Row of matrix A
-    int vB[DIM];            //Column of matrix B
+    int vA[ROW];            //Row of matrix A
+    int vB[COL];            //Column of matrix B
     int result;
-    int x;              //Dimension
+    int x;
+    int i;
+    int j;
 } t_mat;
 
-int A[DIM][DIM];
-int B[DIM][DIM];
-int C[DIM][DIM];
-int flag = 0;
+int A[ROW][COL];
+int B[COL][ROW];
+int C[ROW][ROW];
 
-void vTaskProduct(void *p) {
-    t_mat *pn = (t_mat *)p;
+void vTaskProduct(void *data) {
+    t_mat *p = (t_mat *)data;
 
-    for(int i = 0; i < pn->x; i++)
-        pn->result += pn->vA[i] * pn->vB[i];
+    for(int i = 0; i < p->x; i++) {
+        p->result += p->vA[i] * p->vB[i];
+        printf("%d %d\n", p->vA[i], p->vB[i]);
+    }
+    C[p->i][p->j] = p->result;
+    vTaskDelete(NULL);
 }
 
 void vTaskPrint() {
     int i, j;
 
-    if (flag == 1) {
-        printf("\n");
-        for(i=0; i < DIM; i++){
-            for(j=0; j < DIM; j++){
-                printf("%d ", C[i][j]);
-            }
-            printf("\n");
+    printf("\n");
+    for(i=0; i < DIM; i++){
+        for(j=0; j < DIM; j++){
+            printf("%d ", C[i][j]);
         }
+        printf("\n");
     }
+    vTaskDelete(NULL);
 }
 
-void copyOnVett(int dim, int *v, int option){
-    if(option == 0){
-        for(int i = 0; i < DIM; i++){
-            v[i] = A[dim][i];
+void copyOnVett(int col, int row, int *v, int option){
+    if(option == 0) {
+        for(int i = 0; i < col; i++){
+            v[i] = A[row][i];
         }
     }
     else{
-        for(int i = 0; i < DIM; i++){
-            v[i] = B[i][dim];
+        for(int i = 0; i < row; i++){
+            v[i] = B[i][col];
         }
     }
 }
 
 void demo3() {
-    t_mat data;
+    t_mat data[ROW*ROW];
 
     //Populate matrix
-    for(int i = 0; i < DIM; i++){
-        for(int j = 0; j < DIM; j++){
-            A[i][j] = j+1;
+    for(int i = 0; i < ROW; i++){
+        for(int j = 0; j < COL; j++){
+            A[i][j] = j + 1;
         }
     }
 
-    for(int i = 0; i < DIM; i++){
-        for(int j = 0; j < DIM; j++){
+    for(int i = 0; i < COL; i++){
+        for(int j = 0; j < ROW; j++){
             B[i][j] = j;
         }
     }
 
-    for(int i = 0; i < DIM; i++){
-        copyOnVett(i, data.vA, 0);
-        for(int j = 0; j < DIM; j++){
-            data.result = 0;
-            copyOnVett(j, data.vB, 1);
-            xTaskCreate(vTaskProduct, "Product", configMINIMAL_STACK_SIZE, (void *)&data, tskIDLE_PRIORITY + 1, NULL);
-            C[i][j] = data.result;
+    for (int i = 0; i < ROW*ROW; i++)
+        data[i].x = COL;
+    int c = 0;
+
+    for(int i = 0; i < ROW; i++){
+        copyOnVett(data[c].x, i, data[c].vA, 0);
+        for(int j = 0; j < COL; j++){
+            copyOnVett(j, data[c].x, data[c].vB, 1);
+            data[c].result = 0;
+            data[c].i = i;
+            data[c].j = j; 
+            xTaskCreate(vTaskProduct, "Product", configMINIMAL_STACK_SIZE, (void *)&data[c], configMAX_PRIORITIES - 1, NULL);
+            c++;
         }
-        flag = 1;
     }
 
-    xTaskCreate(vTaskPrint, "Print", configMINIMAL_STACK_SIZE, NULL, tskIDLE_PRIORITY + 2, NULL);
+    xTaskCreate(vTaskPrint, "Print", configMINIMAL_STACK_SIZE, NULL, configMAX_PRIORITIES - 2, NULL);
     
     /* Starting FreeRTOS scheduler */
     vTaskStartScheduler();
